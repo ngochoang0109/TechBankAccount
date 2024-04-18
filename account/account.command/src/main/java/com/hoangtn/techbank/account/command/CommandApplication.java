deda0fa3f7482723a5f5418759cf36fad187ba68
HoangTN <hoangtn0109@gmail.com>
2024-04-18 11:58:15 +0700

    run project with bootRun

--- account/account.command/src/main/java/com/hoangtn/techbank/account/command/CommandApplication.java
+++ account/account.command/src/main/java/com/hoangtn/techbank/account/command/CommandApplication.java
@@ -1,13 +1,32 @@
 package com.hoangtn.techbank.account.command;
 
+import com.hoangtn.techbank.account.command.api.command.*;
+import com.hoangtn.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandApplication {
 
+	private final CommandDispatcher commandDispatcher;
+	private final CommandHandler commandHandler;
+
+	public CommandApplication(CommandDispatcher commandDispatcher,
+							  CommandHandler commandHandler) {
+		this.commandDispatcher = commandDispatcher;
+		this.commandHandler = commandHandler;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
 }
--- account/account.command/src/main/java/com/hoangtn/techbank/account/command/api/command/OpenAccountCommand.java
+++ account/account.command/src/main/java/com/hoangtn/techbank/account/command/api/command/OpenAccountCommand.java
@@ -2,6 +2,7 @@ package com.hoangtn.techbank.account.command.api.command;
 
 import com.hoangtn.techbank.account.common.dto.AccountType;
 import com.hoangtn.techbank.cqrs.core.command.BaseCommand;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -18,4 +19,12 @@ public class OpenAccountCommand extends BaseCommand {
     public OpenAccountCommand(String id) {
         super(id);
     }
+
+    public OpenAccountCommand(String id, String accountHolder, AccountType accountType, double openingBalance) {
+        super(id);
+        this.accountHolder = accountHolder;
+        this.accountType = accountType;
+        this.openingBalance = openingBalance;
+    }
+
 }
